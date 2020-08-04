9be1c6439ba2514cb19ea31be14766c7fd116cd7
guldenmw <guldenmw@gmail.com>
2020-08-05 00:31:36 +0200

    Updates

---
+++ precache-manifest.acd3551d3aa9419b27f627795389d555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd6c1a3d88ae874f99dc5f6c54c513da",
+    "url": "/warp-assessment/index.html"
+  },
+  {
+    "revision": "980708ac80769fb4e654",
+    "url": "/warp-assessment/static/css/main.77c36acc.chunk.css"
+  },
+  {
+    "revision": "5210a4ed5375697c7458",
+    "url": "/warp-assessment/static/js/2.cd7a050c.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/warp-assessment/static/js/2.cd7a050c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "980708ac80769fb4e654",
+    "url": "/warp-assessment/static/js/main.d90c39ad.chunk.js"
+  },
+  {
+    "revision": "a9b73a90d314df5478a7",
+    "url": "/warp-assessment/static/js/runtime-main.3689965d.js"
+  }
+]);
